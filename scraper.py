ee929d1e56799c603cf9a7516399ed4112e1b269
Alexander Berry <alexander_berry@alumni.brown.edu>
2023-02-16 21:58:49 -0500

    revised date collection to include all since 1978

--- scraper.py
+++ scraper.py
@@ -12,18 +12,13 @@ import pathlib
 options = Options()
 options.add_argument('--headless')
 
-# number of years in the past to collect data for
-HISTORICAL_SEARCH_WINDOW = 10
-
 today = date.today()
-latest_start_date = today - timedelta(days=3)
-search_starts = [
-    (latest_start_date - timedelta(days=i*365)).strftime('%m/%d/%Y') for i in range(0,HISTORICAL_SEARCH_WINDOW)
-]
-search_ends = [
-    (latest_start_date - timedelta(days=i*365)).strftime('%m/%d/%Y') for i in range(1,HISTORICAL_SEARCH_WINDOW+1)
-]
-search_intervals = list(zip(search_ends, search_starts))
+current_year = today.year
+latest_start_date = (today - timedelta(days=3)).strftime('%m/%d/%Y')
+all_years = range(1978, current_year-1)
+
+intervals = [(f'01/01/{y}', f'01/01/{y+1}') for y in all_years]
+intervals.append((f'01/01/{current_year}', latest_start_date))
 
 # set up profile to handle downloads
 cwd = pathlib.Path().absolute()
@@ -33,7 +28,7 @@ options.set_preference('browser.download.dir', f'{cwd}/data/raw_data')
 options.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
 
 driver = webdriver.Firefox(options=options)
-progress_bar = tqdm(search_intervals)
+progress_bar = tqdm(intervals)
 for interval in progress_bar:
     progress_bar_desc = 'Downloading year: ' + str(interval)
     progress_bar.set_postfix_str(progress_bar_desc)
@@ -44,8 +39,8 @@ for interval in progress_bar:
     to_date_elem = driver.find_element(By.NAME, "to_date") # Select the "to date" box
     to_date_elem.clear() # Clear the input
     driver.switch_to.alert.accept() # Accept the alert
-    from_date = search_intervals[1]
-    to_date = search_intervals[0]
+    from_date = intervals[0]
+    to_date = intervals[1]
     from_date_elem.send_keys(from_date) # Send the start date
     to_date_elem.send_keys(to_date) # Send the end date
     submit_elem = driver.find_element(By.XPATH, '/html/body/div/div[3]/div/form[2]/div[8]/input[1]')
